f5400c6c355dd7770e00829412191b248ad63e70
dwayhs <danielwayhs@gmail.com>
2017-03-21 09:49:44 -0300

    Add func returning two values

--- 4-funcoes/main.go
+++ 4-funcoes/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 // exemplo de função privada
-func Add(x, y int) int {
-	return x + y
+func Add(x, y int) (int, int) {
+	return x + y, 0
 }
 
 func main() {
-	fmt.Println("O resultado de 10 + 20 é", Add(10, 20))
+  result, other := Add(10, 20)
+	fmt.Println("O resultado de 10 + 20 é", result)
+  fmt.Println("O parametro extra é:", other)
 }

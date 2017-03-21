7efcd1f6a43072b6863b0d968814e815ef70008c
dwayhs <danielwayhs@gmail.com>
2017-03-21 09:45:58 -0300

    Add is now public

--- 4-funcoes/main.go
+++ 4-funcoes/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 // exemplo de função privada
-func add(x int, y int) int {
+func Add(x int, y int) int {
 	return x + y
 }
 
 func main() {
-	fmt.Println("O resultado de 10 + 20 é", add(10, 20))
+	fmt.Println("O resultado de 10 + 20 é", Add(10, 20))
 }
